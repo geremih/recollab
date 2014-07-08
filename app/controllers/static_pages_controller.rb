83e910148bce395075e7a558af7d9d38aa4ff0de
Mihir Rege <mihirrege@gmail.com>
2014-07-08 10:29:13 +0530

    Add cover layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,12 @@
 class StaticPagesController < ApplicationController
-  before_action :authenticate_user!, except: [:home]
+  before_action :authenticate_user!, except: [:home, :help]
+  layout "cover", except: [:help]
   def home
     if signed_in?
       redirect_to user_root_path 
     end
+  end
+
+  def help
   end
 end
