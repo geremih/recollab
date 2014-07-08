d294520a8355966b43d2e3be4173e14d8a5cecfd
Mihir Rege <mihirrege@gmail.com>
2014-07-08 10:27:46 +0530

    Configure Facebook omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -168,7 +168,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
+  config.omniauth :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
